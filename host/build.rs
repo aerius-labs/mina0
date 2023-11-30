6138b734fd25efd80875ae087493ce40b6c94062
Rahul Ghangas <rahulghangas0@gmail.com>
2023-11-30 19:29:09 +0530

    chore: update build flow for reading and writing srs(params)

--- host/build.rs
+++ host/build.rs
@@ -1,38 +1,41 @@
 use std::fs::File;
-use std::{mem, slice};
 use std::io::Write;
 use std::path::Path;
 use kimchi::mina_curves::pasta::Vesta;
 use kimchi::poly_commitment::srs::SRS;
 
-#[repr(C, packed)]
-struct SRSWrapper(SRS<Vesta>);
-
 fn main() {
     // load vesta srs from file
-    let srs_path = std::env::var("SRS_PATH").expect("SRS_PATH env var not set");
+    let srs_path = "../srs".to_string();
 
     // join "vesta.srs" and srs folder path
     let vesta_srs_path = Path::new(&srs_path).join("vesta.srs");
-    let out_path = Path::new(&srs_path).join("vesta_raw");
 
-    // Check if the flag file exists
-    if out_path.exists() {
-        // If the file exists, return early
-        println!("Build script already executed, skipping...");
-        return;
-    }
-
+    // read vesta srs file
     let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
-    let srs = SRSWrapper((rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs")).clone());
+    let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs");
 
-    // write raw memory to a file
-    let obj_ptr: *const SRSWrapper = &srs;
-    let obj_size = mem::size_of::<SRSWrapper>();
-    let obj_slice = unsafe { slice::from_raw_parts(obj_ptr as *const u8, obj_size) };
+    let srs_group_elements_len = srs.g.len();
 
-    // write to file
-    let mut out_file = File::create(out_path).expect("Unable to create output file");
-    out_file.write_all(obj_slice).expect("Failed to write to file");
+    // write vesta field parameters to a json file
+    let vesta_field_params_path = Path::new("build_srs_raw").join("generated_const_params.rs");
+    let mut vesta_field_params_file = File::create(vesta_field_params_path).expect("failed to create vesta field params file");
 
+    // write vesta field as a const to a rust file
+    let vesta_field_params = format!("pub const VESTA_FIELD_PARAMS: usize = {};", srs_group_elements_len);
+    write!(vesta_field_params_file, "{}", vesta_field_params).expect("failed to write vesta field params to file");
+
+
+    let second_build_folder = Path::new("build_srs_raw");
+    assert!(std::env::set_current_dir(&second_build_folder).is_ok());
+
+    // compile and run main.rs in build_srs_raw
+    // let output = std::process::Command::new("cargo")
+    //     .args(&["run", "--manifest-path", "./Cargo.toml"])
+    //     .output()
+    //     .expect("failed to execute second build");
+    //
+    // println!("Status: {}", output.status);
+    // println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+    // println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
