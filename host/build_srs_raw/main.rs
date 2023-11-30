747b200265aeac7eca0bd195291f32b126aa7ffa
Rahul Ghangas <rahulghangas0@gmail.com>
2023-11-30 20:05:13 +0530

    fix: use separate thread with incrased stack size to dump stack allocated srs, resolves stack overflow

--- host/build_srs_raw/main.rs
+++ host/build_srs_raw/main.rs
@@ -6,49 +6,60 @@ use std::fs::File;
 use std::path::Path;
 use std::io::Write;
 use num_traits::identities::Zero;
+use std::thread;
 
 include!("generated_const_params.rs");
-// pub const VESTA_FIELD_PARAMS: usize = 13107;
+// #[derive(Debug)]
 #[repr(C)]
-#[derive(Debug)]
 pub struct SrsSized<G, const N: usize> {
     pub g: [G; N],
     pub h: G
 }
 
 fn main () {
-    // load vesta srs from file
-    let srs_path = "../../srs".to_string();
+    // Define the desired stack size in bytes
+    let stack_size = 32 * 1024 * 1024; // 16 MB
 
-    // join "vesta.srs" and srs folder path
-    let vesta_srs_path = Path::new(&srs_path).join("vesta.srs");
+    // Create a new thread with the specified stack size
+    let builder = thread::Builder::new().stack_size(stack_size);
 
-    let mut srs_sized = SrsSized{
-        g: [Vesta::zero(); VESTA_FIELD_PARAMS],
-        h: Vesta::zero(),
-    };
+    let handler = builder.spawn(|| {
+        // load vesta srs from file
+        let srs_path = "../../srs".to_string();
 
-    {
-        // read vesta srs file
-        let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
-        let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs");
+        // join "vesta.srs" and srs folder path
+        let vesta_srs_path = Path::new(&srs_path).join("vesta.srs");
 
-        // println!("srs_sized: {}", srs_sized.h.to_string());
+        let mut srs_sized = SrsSized{
+            g: [Vesta::zero(); VESTA_FIELD_PARAMS],
+            h: Vesta::zero(),
+        };
 
-        srs_sized.g.copy_from_slice(&srs.g[..VESTA_FIELD_PARAMS]);
-        srs_sized.h = srs.h;
-    }
+        {
+            // read vesta srs file
+            let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
+            let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs");
 
-    // Open a file in write mode
-    let mut file = File::create("../../srs/vesta.bin").unwrap();
+            // println!("srs_sized: {}", srs_sized.h.to_string());
 
-    // Ensure the type is safe to treat as raw bytes
-    assert!(mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>() == mem::size_of_val(&srs_sized));
+            srs_sized.g.copy_from_slice(&srs.g[..VESTA_FIELD_PARAMS]);
+            srs_sized.h = srs.h;
+        }
 
-    // Use unsafe to get the raw bytes of the object
-    unsafe {
-        let byte_ptr = &srs_sized as *const SrsSized<Vesta, VESTA_FIELD_PARAMS> as *const u8;
-        let bytes = std::slice::from_raw_parts(byte_ptr, mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>());
-        file.write_all(bytes).unwrap();
-    }
+        // Open a file in write mode
+        let mut file = File::create("../../srs/vesta.bin").unwrap();
+
+        // Ensure the type is safe to treat as raw bytes
+        assert_eq!(mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>(), mem::size_of_val(&srs_sized));
+
+        // Use unsafe to get the raw bytes of the object
+        unsafe {
+            let byte_ptr = &srs_sized as *const SrsSized<Vesta, VESTA_FIELD_PARAMS> as *const u8;
+            let bytes = std::slice::from_raw_parts(byte_ptr, mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>());
+            file.write_all(bytes).unwrap();
+        }
+    }).unwrap();
+
+    // Wait for the thread to finish executing
+    handler.join().unwrap();
 }
