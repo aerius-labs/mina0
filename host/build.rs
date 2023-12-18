048a8e2e46eac7d3bcb0623830f0df3d57633d3c
Rahul Ghangas <rahulghangas0@gmail.com>
2023-12-18 15:52:32 +0530

    chore: cargo fmt

--- host/build.rs
+++ host/build.rs
@@ -1,8 +1,8 @@
+use kimchi::mina_curves::pasta::Vesta;
+use kimchi::poly_commitment::srs::SRS;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use kimchi::mina_curves::pasta::Vesta;
-use kimchi::poly_commitment::srs::SRS;
 
 fn main() {
     // load vesta srs from file
@@ -13,18 +13,23 @@ fn main() {
 
     // read vesta srs file
     let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
-    let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs");
+    let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file)
+        .expect("failed to deserialize vesta srs");
 
     let srs_group_elements_len = srs.g.len();
 
     // write vesta field parameters to a json file
     let vesta_field_params_path = Path::new("build_srs_raw").join("generated_const_params.rs");
-    let mut vesta_field_params_file = File::create(vesta_field_params_path).expect("failed to create vesta field params file");
+    let mut vesta_field_params_file =
+        File::create(vesta_field_params_path).expect("failed to create vesta field params file");
 
     // write vesta field as a const to a rust file
-    let vesta_field_params = format!("pub const VESTA_FIELD_PARAMS: usize = {};", srs_group_elements_len);
-    write!(vesta_field_params_file, "{}", vesta_field_params).expect("failed to write vesta field params to file");
-
+    let vesta_field_params = format!(
+        "pub const VESTA_FIELD_PARAMS: usize = {};",
+        srs_group_elements_len
+    );
+    write!(vesta_field_params_file, "{}", vesta_field_params)
+        .expect("failed to write vesta field params to file");
 
     let second_build_folder = Path::new("build_srs_raw");
     assert!(std::env::set_current_dir(&second_build_folder).is_ok());
--- host/build_srs_raw/main.rs
+++ host/build_srs_raw/main.rs
@@ -1,11 +1,11 @@
 use kimchi::mina_curves::pasta::Vesta;
 use kimchi::poly_commitment::srs::SRS;
-use rmp_serde;
-use std::mem;
-use std::fs::File;
-use std::path::Path;
-use std::io::Write;
 use num_traits::identities::Zero;
+use rmp_serde;
+use std::fs::File;
+use std::io::Write;
+use std::mem;
+use std::path::Path;
 use std::thread;
 
 include!("generated_const_params.rs");
@@ -13,52 +13,62 @@ include!("generated_const_params.rs");
 #[repr(C)]
 pub struct SrsSized<G, const N: usize> {
     pub g: [G; N],
-    pub h: G
+    pub h: G,
 }
 
-fn main () {
+fn main() {
     // Define the desired stack size in bytes
     let stack_size = 32 * 1024 * 1024; // 32 MB
 
     // Create a new thread with the specified stack size
     let builder = thread::Builder::new().stack_size(stack_size);
 
-    let handler = builder.spawn(|| {
-        // load vesta srs from file
-        let srs_path = "../../srs".to_string();
+    let handler = builder
+        .spawn(|| {
+            // load vesta srs from file
+            let srs_path = "../../srs".to_string();
 
-        // join "vesta.srs" and srs folder path
-        let vesta_srs_path = Path::new(&srs_path).join("vesta.srs");
+            // join "vesta.srs" and srs folder path
+            let vesta_srs_path = Path::new(&srs_path).join("vesta.srs");
 
-        let mut srs_sized = SrsSized{
-            g: [Vesta::zero(); VESTA_FIELD_PARAMS],
-            h: Vesta::zero(),
-        };
+            let mut srs_sized = SrsSized {
+                g: [Vesta::zero(); VESTA_FIELD_PARAMS],
+                h: Vesta::zero(),
+            };
 
-        {
-            // read vesta srs file
-            let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
-            let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file).expect("failed to deserialize vesta srs");
+            {
+                // read vesta srs file
+                let srs_file = File::open(vesta_srs_path).expect("failed to open vesta srs file");
+                let srs = rmp_serde::from_read::<File, SRS<Vesta>>(srs_file)
+                    .expect("failed to deserialize vesta srs");
 
-            // println!("srs_sized: {}", srs_sized.h.to_string());
+                // println!("srs_sized: {}", srs_sized.h.to_string());
 
-            srs_sized.g.copy_from_slice(&srs.g[..VESTA_FIELD_PARAMS]);
-            srs_sized.h = srs.h;
-        }
+                srs_sized.g.copy_from_slice(&srs.g[..VESTA_FIELD_PARAMS]);
+                srs_sized.h = srs.h;
+            }
 
-        // Open a file in write mode
-        let mut file = File::create("../../srs/vesta.bin").unwrap();
+            // Open a file in write mode
+            let mut file = File::create("../../srs/vesta.bin").unwrap();
 
-        // Ensure the type is safe to treat as raw bytes
-        assert_eq!(mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>(), mem::size_of_val(&srs_sized));
+            // Ensure the type is safe to treat as raw bytes
+            assert_eq!(
+                mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>(),
+                mem::size_of_val(&srs_sized)
+            );
 
-        // Use unsafe to get the raw bytes of the object
-        unsafe {
-            let byte_ptr = &srs_sized as *const SrsSized<Vesta, VESTA_FIELD_PARAMS> as *const u8;
-            let bytes = std::slice::from_raw_parts(byte_ptr, mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>());
-            file.write_all(bytes).unwrap();
-        }
-    }).unwrap();
+            // Use unsafe to get the raw bytes of the object
+            unsafe {
+                let byte_ptr =
+                    &srs_sized as *const SrsSized<Vesta, VESTA_FIELD_PARAMS> as *const u8;
+                let bytes = std::slice::from_raw_parts(
+                    byte_ptr,
+                    mem::size_of::<SrsSized<Vesta, VESTA_FIELD_PARAMS>>(),
+                );
+                file.write_all(bytes).unwrap();
+            }
+        })
+        .unwrap();
 
     // Wait for the thread to finish executing
     handler.join().unwrap();
